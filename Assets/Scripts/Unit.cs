c6251a156c360c9456225e2a2af53ab6591db368
gogomaester <stolin.f@gmail.com>
2022-12-05 23:55:48 +0100

    Update Unit.cs

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float stoppingDistance = .05f;
 
     private Vector3 targetPosition;
 
     private void Update()
     {
         // Prevent the unit from moving after it's almost at the position
-        if (Vector3.Distance(transform.position, targetPosition) > 0.05f)
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance) 
         {
             // We calculate normalized direction & then move the unit in that direction
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
