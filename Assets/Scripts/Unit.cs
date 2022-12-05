93bdfb190b0495c5048401149d7c1aedcc86e48a
gogomaester <stolin.f@gmail.com>
2022-12-05 23:50:57 +0100

    Moving unit Test

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 4f;
+
+    private Vector3 targetPosition;
+
+    private void Update()
+    {
+        // Prevent the unit from moving after it's almost at the position
+        if (Vector3.Distance(transform.position, targetPosition) > 0.05f)
+        {
+            // We calculate normalized direction & then move the unit in that direction
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+        }
+
+        // Testing method
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Move(new Vector3(4, 0, 4));
+        }
+    }
+
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+
+}
