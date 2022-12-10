2cfbe88b7ce985a48b755217748e26e8f11cecf1
gogomaester <stolin.f@gmail.com>
2022-12-10 22:08:57 +0100

    Unit Selected Visual, Events

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,28 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class UnitActionSystem : MonoBehaviour
 {
 
-    [SerializeField] private Unit selectedUnit;
+    public event System.EventHandler OnSelectedUnitChanged;
+
     [SerializeField] private LayerMask unitLayer;
 
-    // Start is called before the first frame update
-    void Start()
+    private Unit selectedUnit;
+
+    public Unit getSelectedUnit()
     {
-        
+        return selectedUnit;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // Handle the selection of units, should the unit be selected,
         // do not move it in the same frame
-        if (TryHandleUnitSelection()) return;
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (TryHandleUnitSelection()) return;
+        }
         // Move selectedUnit to a new place after clicking the mouse
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1) && selectedUnit != null)
         {
             selectedUnit.Move(MouseWorld.GetMouseWorldPosition());
         }
@@ -32,17 +37,23 @@ public class UnitActionSystem : MonoBehaviour
     private bool TryHandleUnitSelection()
     {
         // Get mouse button input
-        if (Input.GetMouseButtonDown(0)) {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hitInfo, float.MaxValue, unitLayer))
-            {
-                // Try get the Unit component, if we found it, select the unit
-                if (hitInfo.transform.TryGetComponent<Unit>(out Unit unit)) { 
-                    selectedUnit = unit;
-                    return true;
-                }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, float.MaxValue, unitLayer))
+        {
+            // Try get the Unit component, if we found it, select the unit
+            if (hitInfo.transform.TryGetComponent<Unit>(out Unit unit)) { 
+                SetSelectedUnit(unit); 
+                return true;
             }
-        }
+        } 
+        // Selection was unsuccessful
         return false;
+    }
+
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        // Check whether EventHandler has listeners (otherwise returns null)
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
     }
 }
